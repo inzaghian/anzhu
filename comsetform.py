4ac1e3617da1fc221821c40a99c2573a09a2f315
inzaghian <30223232@qq.com>
2019-08-16 11:05:59 +0800

    change

--- comsetform.py
+++ comsetform.py
@@ -9,34 +9,34 @@ class Comsetwindow(QtWidgets.QWidget):
     _signal = pyqtSignal(dict)
 
     def __init__(self):
-        super(Comsetwindow,self).__init__()
-        self.new=Ui_comsetform()
+        super(Comsetwindow, self).__init__()
+        self.new = Ui_comsetform()
         self.new.setupUi(self)
         self.new.btn_save.clicked.connect(self.Get_set)
 
-    def initcom(self,clist):
+    def initcom(self, clist):
         self.new.cb_com.clear()
         self.new.cb_com.addItems(clist)
         self.setWindowIcon(QIcon('./Icon/dqy.png'))
-        
+
     def Get_set(self):
-        sl={}
-        com=self.new.cb_com.currentText()
-        bsp=self.new.cb_bsp.currentText()
-        d=self.new.cb_data.currentText()
-        p=self.new.cb_p.currentText()
-        s=self.new.cb_stop.currentText()
-        sl={'com':com,'bsp':bsp,'d':d,'p':p,'s':s}
+        sl = {}
+        com = self.new.cb_com.currentText()
+        bsp = self.new.cb_bsp.currentText()
+        d = self.new.cb_data.currentText()
+        p = self.new.cb_p.currentText()
+        s = self.new.cb_stop.currentText()
+        sl = {'com': com, 'bsp': bsp, 'd': d, 'p': p, 's': s}
         self._signal.emit(sl)
         self.close()
         
-    def set_com(self,msg):
+    def set_com(self, msg):
         try:
-            com=msg['com']
-            bsp=msg['bsp']
-            d=msg['d']
-            s=msg['s']
-            p=msg['p']
+            com = msg['com']
+            bsp = msg['bsp']
+            d = msg['d']
+            s = msg['s']
+            p = msg['p']
             self.new.cb_com.setCurrentText(com)
             bsp=self.new.cb_bsp.setCurrentText(bsp)
             d=self.new.cb_data.setCurrentText(d)
